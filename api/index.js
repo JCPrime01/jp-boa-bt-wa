26fcc1a2cc460717d135c2a38af3cde35e085f6f
JCPrime01 <vanzokiller@gmail.com>
2026-06-28 12:33:57 -0300

    chore: shareCode 4OMK9O7IAGY -> NL5FBERJ8OA (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/psawehc2?shareCode=4OMK9O7IAGY&afp=bd&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/psawehc2?shareCode=NL5FBERJ8OA&afp=bd&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
