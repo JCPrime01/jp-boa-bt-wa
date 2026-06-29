f7c5735b948daf82d35d66d5af96af544fc59ace
JCPrime01 <vanzokiller@gmail.com>
2026-06-29 10:17:47 -0300

    chore: shareCode NL5FBERJ8OA -> PV90AL34A2M (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/psawehc2?shareCode=NL5FBERJ8OA&afp=bd&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/psawehc2?shareCode=PV90AL34A2M&afp=bd&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
