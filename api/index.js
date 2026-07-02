8afa2ad39fe7614fdfc8639cc1e5748cf700c770
JCPrime01 <vanzokiller@gmail.com>
2026-07-02 11:45:12 -0300

    chore: shareCode EX1EVNK21MR -> EIEC5HZOV8C (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=EX1EVNK21MR'
+    const shareCode = '&shareCode=EIEC5HZOV8C'
     
     const afp = '&afp=bd'
 
