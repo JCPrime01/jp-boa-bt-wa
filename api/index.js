0271e7a051ad19d04145167e52a9137c8acb09e3
JCPrime01 <vanzokiller@gmail.com>
2026-07-03 21:47:35 -0300

    chore: shareCode WI62K29QYY4 -> RHT6NMSBSD7 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=WI62K29QYY4'
+    const shareCode = '&shareCode=RHT6NMSBSD7'
     
     const afp = '&afp=bd'
 
