f0539b75e15415e27946f80560bd8156bfce15ad
JCPrime01 <vanzokiller@gmail.com>
2026-07-30 11:22:38 -0300

    chore: shareCode 87T7FNKC63E -> ACZEEW0ZCMY (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=87T7FNKC63E'
+    const shareCode = '&shareCode=ACZEEW0ZCMY'
     
     const afp = '&afp=bd'
 
