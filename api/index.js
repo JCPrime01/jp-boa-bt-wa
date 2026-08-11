50d58e93a452cac0ea80f556421dd777aa396a11
JCPrime01 <vanzokiller@gmail.com>
2026-08-11 12:54:53 -0300

    chore: shareCode MD30XOT49W8 -> E6GC9OJMRID (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=MD30XOT49W8'
+    const shareCode = '&shareCode=E6GC9OJMRID'
     
     const afp = '&afp=bd'
 
